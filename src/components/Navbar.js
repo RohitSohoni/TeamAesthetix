5ec4e84b92907bc0e91e42639cb0aafaaee2907d
Rohit Sohoni <rohitsohoni155@gmail.com>
2022-02-27 15:56:58 +0000

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <div className="background">
+            <div className="image">
+                <img src="./Mask Group 1.png" alt=""/>
+            </div>
+            <div className="navbar">
+                <div className="logo">
+                    <img src="./Final white-07.png" alt=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
